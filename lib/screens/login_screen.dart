95fbcbeebcb9aabf2b42ec3f13545bc4c620af47
sorryma3er <whc0713air@gmail.com>
2025-07-08 14:55:06 +0700

    Changed Login & Register into StatefulWidget and used TextEditingController to hold the TextFiled. Not yet integrate with firebase.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginScreenState();
+  }
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  /// use TextEditingController to give the text field a controller, so that:
+  /// enable immediate read & write to it
+  /// auto fill after register
+  /// have listener that can change widget state when text changes, meaning if the format wrong, maybe surrounding can show red to user
+  final emailController = TextEditingController();
+  final pwdController = TextEditingController();
+
+  void _onPressedLogin() {
+    final email = emailController.text;
+    final pwd = pwdController.text;
+    //TODO: cooperate with firebase
+    debugPrint('Login pressed: email: $email, pwd: $pwd');
+  }
+
+  @override
+  void dispose() { /// comes in pair with Controller, dispose it to avoid memory leak
+    emailController.dispose();
+    pwdController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +38,46 @@ class LoginScreen extends StatelessWidget {
         title: const Text('Login'),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/register');
-            },
-            child: const Text('Go to register')
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            TextField(
+              controller: pwdController,
+              obscureText: true, /// make it not visible
+              decoration: InputDecoration(
+                labelText: 'PassWord',
+                hintText: 'Enter your password',
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 16,),
+
+            ElevatedButton(
+              onPressed: _onPressedLogin,
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 12),
+            
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.pushNamed(context, '/register'),
+              },
+              child: const Text('Don\'t have an account? Register now'),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
   
+  @override
+  State<StatefulWidget> createState() {
+    return _RegisterScreenState();
+  }
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  /// create controller as same as Login screen
+  final emailController = TextEditingController();
+  final pwdController = TextEditingController();
+
+  void onPressedRegister() {
+    final email = emailController.text;
+    final pwd = pwdController.text;
+    //TODO: cooperate with firebase
+    debugPrint('Register pressed: email: $email, pwd: $pwd');
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    pwdController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,14 +35,49 @@ class RegisterScreen extends StatelessWidget {
         title: const Text('Register'),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/login');
-          },
-          child: Text('Back to Login...'),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: 16,),
+
+            TextField(
+              controller: pwdController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 16,),
+
+            ElevatedButton(
+              onPressed: onPressedRegister,
+              child: const Text('Register'),
+            ),
+            const SizedBox(height: 12,),
+            
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.pop(context),
+              },
+              child: const Text('Already have an account? Login now'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
