9871c3f47da610fe80308559da0c2ca65943c5c9
sorryma3er <whc0713air@gmail.com>
2025-07-15 16:41:52 +0700

    Created basic skeleton of onboarding screen. Used page view to define 2 pages for now, left with tutorial part. Planning to let user fill in the display name & avatar pic in the first page of onboarding.

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final _pageController = PageController();
+  final _displayNameController = TextEditingController(); // store display name
+  XFile? _avatar; // store avatar
+  int _currentPage = 0;
+  bool _showProfileError = false;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _displayNameController.dispose();
+    super.dispose();
+  }
+
+  Future<void> pickAvatar() async {
+    final picker = ImagePicker();
+    final file = await picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      setState(() {
+        _avatar = file;
+      }); // update the avatar
+    }
+  }
+
+  Future<void> completeOnboarding() async {
+    final prefs = await SharedPreferences.getInstance();
+    
+    await prefs.setBool("completeOnboarding", true); // set flag
+    // TODO connect to firebase to set display name and avatar
+
+    // TODO navigate to home screen
+    debugPrint("Navigate to home screen");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              onPageChanged: _handlePageViewChanged,
+              physics: _pageScrollPhysics(),
+              children: [
+                _buildProfilePage(),
+
+                // TODO onboarding tutorial
+                Center(child: Text("Tutorial"),)
+              ],
+            ),
+
+            Positioned(
+              bottom: 24, left: 24, right: 24,
+              child: Row(
+                children: [
+                  // prev button
+                  if (_currentPage > 0)
+                    TextButton(
+                      onPressed: () {
+                        _pageController.previousPage(duration: const Duration(milliseconds: 300), curve: Curves.easeInOut),
+                      },
+                      child: const Text("Prev"),
+                    )
+                  else
+                    const SizedBox(width: 40,),
+
+                  // smooth indicator
+                  SmoothPageIndicator(
+                    controller: _pageController,
+                    count: 2,
+                    effect: ExpandingDotsEffect(
+                      activeDotColor: Colors.lightBlueAccent,
+                      dotColor: Colors.white54,
+                      dotHeight: 8,
+                      dotWidth: 8,
+                    ),
+                  ),
+
+                  // next / complete button
+                  TextButton(
+                    onPressed: () {
+                      if (_currentPage == 0) {
+                        if (!_profileInfoComplete) {
+                         setState(() {
+                           _showProfileError = true;
+                         });
+                         return; // short circuit this function
+                        }
+                      }
+
+                      if (_currentPage == 1) {
+                        completeOnboarding();
+                      } else {
+                        _pageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+                      }
+                    },
+                    child: Text(_currentPage == 1 ? "Complete" : "Next"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handlePageViewChanged(int page) {
+    setState(() {
+      _currentPage = page;
+    });
+  }
+
+  Widget _buildProfilePage() {
+
+  }
+
+  ScrollPhysics _pageScrollPhysics() {
+    if (_currentPage == 0 && !_profileInfoComplete) { // disable scroll when acc info not complete
+      return const NeverScrollableScrollPhysics();
+    } else {
+      return const BouncingScrollPhysics();
+    }
+  }
+
+  bool get _profileInfoComplete {
+    return _displayNameController.text.isNotEmpty && _avatar != null;
+  }
+
+}
