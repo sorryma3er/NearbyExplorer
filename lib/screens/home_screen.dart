4c1613b23a9bea49d5068b2b953b10686be038b5
sorryma3er <whc0713air@gmail.com>
2025-07-18 13:32:09 +0700

    Added HomeScreen to import all four pages, and put them into the bottomNavigationBar, which acts as tab bar.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import './pages/explore_page.dart';
+import './pages/notifications_page.dart';
+import './pages/favorites_page.dart';
+import './pages/profile_page.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  /// track which tab is selected
+  int _currentIndex = 1; // default to be 1 --- Explore Page
+
+  static final List<Widget> _pages = <Widget>[
+    const ExplorePage(),
+    const NotificationsPage(),
+    const FavoritesPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.grey,
+        onTap: (i) => setState(() {
+          _currentIndex = i;
+        }),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Explore'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+
+      ),
+    );
+  }
+}
